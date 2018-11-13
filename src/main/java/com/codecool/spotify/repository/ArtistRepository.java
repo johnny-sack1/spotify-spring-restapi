74ba9856d07fe2334c68822aa94971542b9d44de
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-13 14:47:08 +0100

    Change ArtistRepository extends CrudRepository -> JpaRepository

--- src/main/java/com/codecool/spotify/repository/ArtistRepository.java
+++ src/main/java/com/codecool/spotify/repository/ArtistRepository.java
@@ -6,7 +6,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface ArtistRepository extends CrudRepository<Artist, Long> {
+@Repository
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
     List<Artist> findAll();
     Optional<Artist> findById(int id);
 }
