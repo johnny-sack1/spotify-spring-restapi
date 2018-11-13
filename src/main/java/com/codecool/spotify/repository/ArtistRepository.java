f38e67bb2e27a35993f4c9d091b335af38401808
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-13 14:45:49 +0100

    Add findById/getArtistById in Artist repo & service

--- src/main/java/com/codecool/spotify/repository/ArtistRepository.java
+++ src/main/java/com/codecool/spotify/repository/ArtistRepository.java
@@ -2,8 +2,11 @@ package com.codecool.spotify.repository;
 
 import com.codecool.spotify.model.Artist;
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
     List<Artist> findAll();
+    Optional<Artist> findById(int id);
 }
--- src/main/java/com/codecool/spotify/service/ArtistService/ArtistService.java
+++ src/main/java/com/codecool/spotify/service/ArtistService/ArtistService.java
@@ -3,6 +3,7 @@ package com.codecool.spotify.service.ArtistService;
 import com.codecool.spotify.model.Artist;
 import com.codecool.spotify.repository.ArtistRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +15,9 @@ public class ArtistService {
 
     public List<Artist> getAll() {
         return artistRepository.findAll();
+    }
+
+    public Optional<Artist> getArtistById(int id) {
+        return artistRepository.findById(id);
     }
 }
