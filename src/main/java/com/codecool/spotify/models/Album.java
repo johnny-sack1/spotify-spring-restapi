45e6e1d1aa220f9a6c787f54f4a9b01273113634
johnny-sack1 <jonymaka@gmail.com>
2018-11-13 13:45:55 +0100

    Add "price" field

--- src/main/java/com/codecool/spotify/models/Album.java
+++ src/main/java/com/codecool/spotify/models/Album.java
@@ -1,24 +1,27 @@
 package com.codecool.spotify.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name="album")
 public class Album {
     private long id;
+    private int price;
     private String title;
-    private Artist artist;
 
-    public Album(String title, Artist artist) {
+    public Album(long id, int price, String title) {
+        this.id = id;
+        this.price = price;
         this.title = title;
-        this.artist = artist;
     }
 
     public Album() {}
 
     @Id
     @GeneratedValue
+    @NotNull
+    @Column(name="id", unique=true)
     public long getId() {
         return id;
     }
@@ -27,19 +30,21 @@ public class Album {
         this.id = id;
     }
 
+    @Column(name="price")
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Column(name="title")
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public Artist getArtist() {
-        return artist;
-    }
-
-    public void setArtist(Artist artist) {
-        this.artist = artist;
     }
 }
