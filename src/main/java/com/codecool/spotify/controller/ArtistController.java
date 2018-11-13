b3b0e333dce8169f662187ed535a68d2a5601ada
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-13 14:48:18 +0100

    Add & implement ArtistController w/ getAll & getArtistById methods

---
+++ src/main/java/com/codecool/spotify/controller/ArtistController.java
@@ -0,0 +1,28 @@
+package com.codecool.spotify.controller;
+
+import com.codecool.spotify.model.Artist;
+import com.codecool.spotify.service.ArtistService.ArtistService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "artist")
+public class ArtistController {
+
+    @Autowired
+    ArtistService artistService;
+
+    @GetMapping(produces = "application/json")
+    public List<Artist> getAll() {
+        return artistService.getAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public Artist getArtistById(@PathVariable int id) {
+        return artistService.getArtistById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+}
