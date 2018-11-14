4c0a2d5217d0c2cc205a4f346529940a1c312137
johnny-sack1 <jonymaka@gmail.com>
2018-11-14 09:47:34 +0100

    Implement GET and DELETE method handlers

--- src/main/java/com/codecool/spotify/controllers/AlbumController.java
+++ src/main/java/com/codecool/spotify/controllers/AlbumController.java
@@ -18,13 +18,32 @@ public class AlbumController {
         return repository.findAll();
     }
 
+    @GetMapping("/albums/{id}")
+    public Album one(@PathVariable long id) {
+        return repository.findById(id).orElseThrow(() -> new AlbumNotFoundException(id));
+    }
+
     @PostMapping(value = "/albums")
     public Album newAlbum(@RequestBody Album newAlbum) {
         return repository.save(newAlbum);
     }
 
-    @GetMapping("/albums/{id}")
-    public Album one(@PathVariable long id) {
-        return repository.findById(id).orElseThrow(() -> new AlbumNotFoundException(id));
+    @PutMapping("/albums/{id}")
+    public Album replaceAlbum(@RequestBody Album newAlbum, @PathVariable long id) {
+        return repository.findById(id)
+                .map(album -> {
+                    album.setTitle(newAlbum.getTitle());
+                    album.setPrice(newAlbum.getPrice());
+                    return repository.save(newAlbum);
+                })
+                .orElseGet(() -> {
+                    newAlbum.setId(id);
+                    return repository.save(newAlbum);
+                });
+    }
+
+    @DeleteMapping("albums/{id}")
+    public void deleteAlbum(@PathVariable long id) {
+        repository.deleteById(id);
     }
 }
